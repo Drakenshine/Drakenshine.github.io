7d7a71c8dc1ae8c4118dc7dfad66194a781b746b
Drakenshine <Drakenshine@gmail.com>
2017-07-12 20:01:06 -0700

    test

---
+++ js/test.js
@@ -0,0 +1,12 @@
+window.prompt("What is your name?");
+
+
+function myFunction() {
+    var txt;
+    var person = prompt("Enter Name", "");
+    if (person == null || person == "") {
+        txt = "User chose not to enter their name.";
+    } else {
+        txt = "Welcome" + person + "!";
+    }
+}
