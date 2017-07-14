c78309a9ee3162d0c6384efc414236bf6866b5e6
Drakenshine <Drakenshine@gmail.com>
2017-07-13 21:28:24 -0700

    1

--- js/mod6.js
+++ js/mod6.js
@@ -1,4 +1,4 @@
-var msg = '<p>Week 6 - Weekly Assignment </p>';
+var msg = '<p>Week 6 - Weekly Assignment </p> <br />';
 msg += '<p><b>page title: </b>' + document.title + '<br />';
 msg += '<b>page address: </b>' + document.URL + '<br />';
 msg += '<b>last modified: </b>' + document.lastModified + '</p>';
