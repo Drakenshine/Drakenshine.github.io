2c5d5380100d57cc96ddfeaa622a8b2fecb376db
Drakenshine <Drakenshine@gmail.com>
2017-07-13 20:59:23 -0700

    test

---
+++ js/mod6.js
@@ -0,0 +1,6 @@
+var msg = ' Week 6 - Weekly Assignment';
+msg += '<p><b>page title: </b>' + document.title + '<br />';
+msg += '<b>page address: </b>' + document.URL + '<br />';
+msg += '<b>last modified: </b>' + document.lastModified + '</p>';
+var el = document.getElementById('footer');
+el.innerHTML = msg;
