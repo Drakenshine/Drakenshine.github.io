0f57f63c1203555e6f0dab0c8b6bb472fb98f4dc
Drakenshine <Drakenshine@gmail.com>
2017-07-22 11:14:15 -0700

    1

--- js/mod7.js
+++ js/mod7.js
@@ -30,7 +30,3 @@ var details3 = sunsetHotel.name + ' rooms: ';
     details3 += sunsetHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details2;
-/* 
-NOTE: textContent does not work in IE8 or earlier
-You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
-*/
