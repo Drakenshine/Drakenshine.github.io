bcfc88fb3b20c4542e863cb5cd785078ef3ba3e0
Drakenshine <Drakenshine@gmail.com>
2017-07-26 13:43:35 -0700

    1

--- js/artifact04.js
+++ js/artifact04.js
@@ -107,20 +107,20 @@ function ValidateForm(){
  //!myContact.countries.value.match(letters))
   
        /*********** VALIDATES ZIP CODE ******** */
- //if (myContact.zipcode.value==null ||
- //myContact.zipcode.value=== "" ||
- //myContact.zipcode.value.length >5 ||
- //myContact.countries.value==="United States" &&
- //myContact.zipcode.value.match(letters))
+ if (myContact.zipcode.value==null ||
+ myContact.zipcode.value=== "" ||
+ myContact.zipcode.value.length >5 ||
+ myContact.countries.value==="United States" &&
+ myContact.zipcode.value.match(letters))
  
 
-if (myContact.countries.value== "Canada" &&
-myContact.zipcode.value=== "" ||
-myContact.countries.value=="Mexico" &&
-myContact.zipcode.value=== "" ||
-myContact.countries.value==="United States" &&
-myContact.zipcode.value=== "" &&
-myContact.zipcode.value.length >5)
+//if (myContact.countries.value== "Canada" &&
+//myContact.zipcode.value=== "" ||
+//myContact.countries.value=="Mexico" &&
+//myContact.zipcode.value=== "" ||
+//myContact.countries.value==="United States" &&
+//myContact.zipcode.value=== "" &&
+//myContact.zipcode.value.length >5)
 
 ) 
 errorMessages += "Zip Code field is required."
