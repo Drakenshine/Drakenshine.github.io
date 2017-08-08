9f1369be2595bb5599fe4f5e4cc136265a99e5f8
Drakenshine <Drakenshine@gmail.com>
2017-08-07 22:34:46 -0700

    2

--- js/datemod.js
+++ js/datemod.js
@@ -1,3 +1,15 @@
-msg += '<b>last modified: </b>' + document.lastModified + '</p>';
-var el = document.getElementById('footer');
-el.innerHTML = msg;
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+		today;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+    return i;
+}
